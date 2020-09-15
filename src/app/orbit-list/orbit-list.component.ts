f3ea13ace685d56fc882a1dba95450007e5cc8a0
Haley Lane <67027117+haleylane@users.noreply.github.com>
2020-09-15 13:00:55 -0500

    part 4.2

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -7,6 +7,7 @@ import { Satellite } from '../satellite';
 })
 export class OrbitListComponent implements OnInit {
   @Input() satellites: Satellite[];
+  
   constructor() { }
 
   ngOnInit() {
--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -5,7 +5,7 @@ export class Satellite {
     operational: boolean;
     launchDate: string;
 
-    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {}
-    
-
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        /* assign class properties in the constructor 5a and b*/
+    }
 }
