26f99c58c5f2f9e126daae48cbc8ce500cee86ba
Haley Lane <67027117+haleylane@users.noreply.github.com>
2020-09-19 13:59:28 -0500

    7.3

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -10,10 +10,25 @@ export class OrbitListComponent implements OnInit {
   @Input() satellites: Satellite[];
 
   constructor() { 
+
+
           /*let showWarning: boolean = shouldShowWarning();
           console.log(showWarning);*/
           //let showWarning: boolean = shouldShowWarning();
   }
+  //7.3 is below
+  sort(column: string): void {
+    // array.sort modifies the array, sorting the items based on the given compare function
+    this.satellites.sort(function(a: Satellite, b: Satellite): number {
+       if(a[column] < b[column]) {
+          return -1;
+       } else if (a[column] > b[column]) {
+          return 1;
+       }
+       return 0;
+    });
+ }
+ //end of 7.3
 
   ngOnInit() {
   }
